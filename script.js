e5f17dc4909e0b7dae03f2bae0f81a01dd69c462
rt2171998 <70574052+rt2171998@users.noreply.github.com>
2023-06-15 14:06:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
-function threeSum(arr, target) {
-// write your code here
-  
+function threeSum(nums, n) {
+  const totalSum = (n * (n + 1)) / 2;
+  let arraySum = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    arraySum += nums[i];
+  }
+
+  const missingNumber = totalSum - arraySum;
+  return missingNumber;
 }
+
+// Example usage
+
+
 
 module.exports = threeSum;
